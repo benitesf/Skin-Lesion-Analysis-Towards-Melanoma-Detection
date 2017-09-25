fd77b9d185fde2130eaf24bab9b5f808d416855e
benitesf <edzon_bf@hotmail.com>
2017-09-25 16:35:35 +0200

    Modificación del shades_of_gray

--- preprocessing/shades_of_gray.py
+++ preprocessing/shades_of_gray.py
@@ -1,7 +1,9 @@
 from math import pow
-from scipy.misc import imread
+from scipy.misc import imread, imsave
 from skimage import img_as_float, img_as_int
+from skimage import exposure
 import numpy as np
+import random
 
 import sys, os
 sys.path.append("/home/linux1/Escritorio/Skin-Lesion-Analysis-Towards-Melanoma-Detection/")
@@ -10,8 +12,11 @@ os.chdir("/home/linux1/Escritorio/Skin-Lesion-Analysis-Towards-Melanoma-Detectio
 melanoma_path = 'image/ISIC-2017_Training_Data_Clean/'
 melanoma_extension = 'jpg'
 
-img = imread(melanoma_path + 'ISIC_0000025.jpg')
+img = imread(melanoma_path + 'ISIC_0014797.jpg')
 img_new = img_as_float(img)
+
+#gamma = random.gauss(1, 0.1)
+img = exposure.adjust_gamma(img, gamma=2.2)
 
 """
 Illuminant estimated using Minkowski norm
@@ -35,7 +40,6 @@ img_new[:,:,0] = img_new[:,:,0]*d[0]
 img_new[:,:,1] = img_new[:,:,1]*d[1]
 img_new[:,:,2] = img_new[:,:,2]*d[2]
 
-import scipy.misc
+imsave('Testing14797.jpeg', img_new)
 
-scipy.misc.imsave('Testing14521.jpg', img_new)
 #I = np.identity(3, dtype=float)*e
