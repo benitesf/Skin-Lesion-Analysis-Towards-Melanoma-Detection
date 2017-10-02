09b15116c60ea0ad62d3ee51677c86a61ced8df0
benitesf <edzon_bf@hotmail.com>
2017-10-02 19:39:30 +0200

    Parámetros para test

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,8 @@
 from scipy.misc import imread, imsave
 import util.dirhandler
 from preprocessing.contrast_enhancement import median_filter_, shading_attenuation_method
-from preprocessing.shades_of_gray import shades_of_gray_method
+from preprocessing.color_augmentation import shades_of_gray_method
+import os
 #import sys, os
 #sys.path.append("/home/mrobot/Documentos/TFG/code/Skin-Lesion-Analysis-Towards-Melanoma-Detection/")
 #os.chdir("/home/mrobot/Documentos/TFG/code/Skin-Lesion-Analysis-Towards-Melanoma-Detection/")
@@ -26,15 +27,19 @@ Iterate over the list of images
 """
 
 for img_name in all_melanoma:
+    print('Preprocessing: '+img_name)
     # Get the image
     image = imread(melanoma_path + img_name)
 
+    print('\tfiltering...')
     # Median filter
     image = median_filter_(image)
 
+    print('\tweaking shadows...')
     # weak effect of nonuniform illumination or shadow
     image = shading_attenuation_method(image, extract=40, margin=10)
 
+    print('\tcolor augmentation...')
     # Color augmentation
     image = shades_of_gray_method(image)
     imsave('image/Preprocessed_Data/'+img_name, image)
