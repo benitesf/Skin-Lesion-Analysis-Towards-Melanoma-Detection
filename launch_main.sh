b0e6a41ffaeaff78ce456d1a02e64e09be0e141f
benitesf <edzon_bf@hotmail.com>
2017-11-28 11:28:35 +0100

    Actualización del launch para el cluster

--- launch_main.sh
+++ launch_main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -N segmentation
 #$ -q lola
-#$ -e salidas/err/segmentation_red1_pre_err_test.txt
-#$ -o salidas/out/segmentation_red1_pre_out_test.txt
+#$ -e salidas/err/red1/segmentation_err.txt
+#$ -o salidas/out/red1/segmentation_out.txt
 #$ -cwd
 
 /anaconda/anaconda3/bin/python3 main.py
