ef890095af0eb786203257d40bd6816decb4e7f5
benitesf <edzon_bf@hotmail.com>
2017-11-28 15:47:40 +0100

    Actualizacion de parametros para ejecutar el cluster con las imagenes preprocesadas

--- launch_main.sh
+++ launch_main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -N segmentation
 #$ -q lola
-#$ -e salidas/err/red1/segmentation_err.txt
-#$ -o salidas/out/red1/segmentation_out.txt
+#$ -e salidas/err/red1/segmentation_pre_err.txt
+#$ -o salidas/out/red1/segmentation_pre_out.txt
 #$ -cwd
 
 /anaconda/anaconda3/bin/python3 main.py
